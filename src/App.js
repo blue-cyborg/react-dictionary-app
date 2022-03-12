ed56b135521159151cdccf4010845aff983e3b06
blue-cyborg <87626094+blue-cyborg@users.noreply.github.com>
2022-03-11 22:10:23 -0500

    followed search engine vid

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
+import Dictionary from "./Dictionary"
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
-        Hello World!
-
+        Hello World! this could be an image or funky text
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        Coded by Candice signature
+      </footer>
+      </div>
     </div>
   );
 }
