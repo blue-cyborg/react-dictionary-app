63e3bef6f75c186fac8f73a6926e8d46d56f2448
blue-cyborg <87626094+blue-cyborg@users.noreply.github.com>
2022-03-16 22:09:27 -0400

    pictures api
    some css tweaks plus adding the pictures api. it now shows pictures!

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,20 +1,33 @@
 import React, {useState} from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
     let [keyword, setKeyword] = useState(props.defaultKeyword);
     let [results, setResults] = useState(null);
     let [loaded, setLoaded] = useState(false);
+    let [photos, setPhotos] = useState(null);
     
-    function handleResponse(response) {
+    function handleDictionaryResponse(response) {
         setResults(response.data[0]);
+    }
+
+    function handlePexelsResponse(response) {
+        setPhotos(response.data.photos);
     }
 
     function search() {
          let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-        axios.get(apiUrl).then(handleResponse);
+        axios.get(apiUrl).then(handleDictionaryResponse);
+
+        let pexelsApiKey = 
+        "563492ad6f91700001000001e56b4d89891a4598858a322f509c40c6";
+        let pexelsApiUrl =
+        `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+        let headers = {"Authorization" : `Bearer ${pexelsApiKey}`};
+        axios.get(pexelsApiUrl, { headers: headers}).then(handlePexelsResponse);
     }
     
     function handleSubmit(event) {
@@ -41,6 +54,7 @@ export default function Dictionary(props) {
                 <input type="submit" value="Search🔍" className="btn btn-primary" />
             </form>
         <Results  results={results} />
+        <Photos photos={photos} />
     </div>
     );
     } else {
