85587fee82d2cf422ce3288daec090f5bd497571
blue-cyborg <87626094+blue-cyborg@users.noreply.github.com>
2022-03-16 22:25:33 -0400

    Update App.js
    changed my default keyword to turtle. previous word was query. There were no pics for query. I wanted a word that shows pics

--- src/App.js
+++ src/App.js
@@ -6,38 +6,38 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-      <header className="App-header">
-        <h1>
-          Dictionary
-        </h1>
-        <h2>
-          Find the word that has you stumped...
-        </h2>
-        <Reading />
-      </header>
-      <main>
-        <Dictionary defaultKeyword="query" />
-      </main>
-      <footer className='App-footer'>
-      <div className="d-flex justify-content-center">
-          <p>
-            <span role="img" aria-label="robot">
-              🤖
-            </span>
-            Open source code
-            <a
-              href="https://github.com/blue-cyborg/react-dictionary-app"
-              target="_blank"
-              rel="noreferrer"
-            >
-              {""} made by Candice Drave
+        <header className="App-header">
+          <h1>
+            Dictionary
+          </h1>
+          <h2>
+            Find the word that has you stumped...
+          </h2>
+          <Reading />
+        </header>
+        <main>
+          <Dictionary defaultKeyword="turtle" />
+        </main>
+        <footer className='App-footer'>
+          <div className="d-flex justify-content-center">
+            <p>
               <span role="img" aria-label="robot">
                 🤖
               </span>
-            </a>
-          </p>
-        </div>
-      </footer>
+              Open source code
+              <a
+                href="https://github.com/blue-cyborg/react-dictionary-app"
+                target="_blank"
+                rel="noreferrer"
+              >
+                {""} made by Candice Drave
+                <span role="img" aria-label="robot">
+                  🤖
+                </span>
+              </a>
+            </p>
+          </div>
+        </footer>
       </div>
     </div>
   );
