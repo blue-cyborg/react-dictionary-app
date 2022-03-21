1df85c3242d7c42ac17affde4aaf4de85c394834
blue-cyborg <87626094+blue-cyborg@users.noreply.github.com>
2022-03-20 21:28:45 -0400

    the word example
    Only show the word example when there is one

--- src/Meaning.js
+++ src/Meaning.js
@@ -13,7 +13,6 @@ export default function Meaning(props) {
                         <p>
                             <strong>Definition:</strong> {definition.definition}
                             <br />
-                            <strong>Example:</strong>
                             <em>{definition.example}</em>
                             <br />
                             <Synonyms synonyms={definition.synonyms} />
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -3,13 +3,18 @@ import React from "react";
 export default function Synonyms(props) {
     if (props.synonyms) {
        return (
-           <ul className="Synonyms">
+           <div className="Synonyms">
         {props.synonyms.map(function(synonym, index) {
             return (
+                <div>
+                <strong>Example:</strong>
+                <ul>
                 <li key={index}>{synonym}</li>
+                </ul>
+                </div>
             )
         })}
-        </ul>
+        </div>
        );
     } else {
         return null;
